e275c89c4a9b87c140ba0b20c0ee33e444fd7b9e
JorgeGA17 <jorgegarciaalejo@gmail.com>
2024-07-17 17:49:10 -0500

    17/07

--- src/app/Proyecto/lista-proyectos/lista-proyectos.component.ts
+++ src/app/Proyecto/lista-proyectos/lista-proyectos.component.ts
@@ -6,23 +6,42 @@ import { ProyectoService } from '../../service/proyecto.service';
 @Component({
   selector: 'app-lista-proyectos',
   templateUrl: './lista-proyectos.component.html',
-  styleUrls: ['./lista-proyectos.component.css']
-})
-export class ListaProyectosComponent implements OnInit {
+  styleUrls: ['./lista-proyectos.component.css'],
  
-  proyectos: Proyecto[];
+})
+export class ListaProyectosComponent implements OnInit{
 
+  proyectos: Proyecto[] = [];
+  page: number = 1;
+  noOfRows: number=10;
+  datas:any;
   constructor(private proyectoService: ProyectoService) {}
 
   ngOnInit(): void {
-    this.getProyectos();
+      this.getProyectos();
   }
 
 
   private getProyectos(): void {
     this.proyectoService.getAllProyectos().subscribe(data => {
       this.proyectos = data;
-  
     });
   }
+  
+  getStartIndex(currentPage: number, lastPage:number){
+    let firstIndex=1;
+    if((currentPage!== lastPage)||(currentPage>0 && lastPage>0)){
+      firstIndex=(Number(this.noOfRows)*(Number(currentPage)-1)+1)
+    }
+    return firstIndex.toString()
+    }
+
+    getLastIndex(currentPage: number, lastPage:number){
+      let lastIndex=this.datas ? this.datas.length: null;
+      if((currentPage!== lastPage)){
+        lastIndex=(Number(this.noOfRows)*(Number(currentPage)))
+      }
+      return lastIndex.toString()
+      }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,18 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ListaProyectosComponent } from './Proyecto/lista-proyectos/lista-proyectos.component';
-import { HttpClientModule } from '@angular/common/http';
-import { ListaCortesComponent } from './Corte/lista-cortes/lista-cortes.component';
-
+import { HttpClientModule } from "@angular/common/http";
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { DataTablesModule } from "angular-datatables";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { ListaCortesComponent } from "./Corte/lista-cortes/lista-cortes.component";
+import { ListaProyectosComponent } from "./Proyecto/lista-proyectos/lista-proyectos.component";
+import {MatCardModule} from '@angular/material/card';
+import {MatTableModule} from '@angular/material/table';
+import {NgxPaginationModule} from 'ngx-pagination'; // <-- import the module
+import { FormsModule } from "@angular/forms";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import {MatMenuModule} from '@angular/material/menu';
 @NgModule({
   declarations: [
     AppComponent,
@@ -17,8 +23,17 @@ import { ListaCortesComponent } from './Corte/lista-cortes/lista-cortes.componen
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    DataTablesModule,
+    MatCardModule,
+    MatTableModule,
+    NgxPaginationModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatMenuModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
