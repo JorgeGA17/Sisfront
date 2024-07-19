29ab701760090312d7f6a0564faae6cd6b7f9a07
JorgeGA17 <jorgegarciaalejo@gmail.com>
2024-07-19 16:43:53 -0500

    home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListaProyectosComponent } from './Proyecto/lista-proyectos/lista-proyectos.component';
 import { ListaCortesComponent } from './Corte/lista-cortes/lista-cortes.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
 
-  { path: 'lista-proyectos', component:ListaProyectosComponent},
-  {path:'',redirectTo:'/lista-proyectos',pathMatch:'full'},
-
+  { path: 'home', component:HomeComponent},
+  {path:'',redirectTo:'/home',pathMatch:'full'},
+  
+  {path: 'lista-proyectos', component: ListaProyectosComponent},
   {path: 'lista-cortes', component: ListaCortesComponent}
 
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,11 +13,15 @@ import { FormsModule } from "@angular/forms";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import {MatMenuModule} from '@angular/material/menu';
+import { HomeComponent } from './home/home.component';
+import { SliderComponent } from './home/slider/slider.component';
 @NgModule({
   declarations: [
     AppComponent,
     ListaProyectosComponent,
-    ListaCortesComponent
+    ListaCortesComponent,
+    HomeComponent,
+    SliderComponent
   ],
   imports: [
     BrowserModule,
